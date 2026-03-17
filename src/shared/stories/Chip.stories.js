3c2953d03e902523da698a0051e1102d4234975a
lareina7486 <lareina7486@gmail.com>
2026-03-18 03:03:06 +0900

    feat: Chip 컴포넌트 (#13)

---
+++ src/shared/stories/Chip.stories.js
@@ -0,0 +1,38 @@
+import { Chip } from '@/shared/components';
+
+export default {
+  title: 'Components/Chip',
+  component: Chip,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+// Object 형태로도 가져와보기
+
+export const Type = () => (
+  <div style={{ display: 'flex', gap: '8px' }}>
+    <Chip type="next" />
+    <Chip type="api" />
+    <Chip type="career" />
+    <Chip type="modernJs" />
+    <Chip type="web" />
+  </div>
+);
+
+export const Category = () => (
+  <div style={{ display: 'flex', gap: '8px' }}>
+    <Chip category="docs" />
+    <Chip category="blog" />
+  </div>
+);
+
+export const Status = () => (
+  <div style={{ display: 'flex', gap: '8px' }}>
+    <Chip status="wait" />
+    <Chip status="rejected" />
+    <Chip status="approved" />
+    <Chip status="deleted" />
+  </div>
+);
