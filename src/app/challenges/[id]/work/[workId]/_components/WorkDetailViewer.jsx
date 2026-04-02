60fb82e76a81ab64d50ac7bffbae87f542826e66
DevWoojin97 <dnekdnwls@gmail.com>
2026-04-02 16:11:25 +0900

    fix: 에디터 css를 기존에 있던 아이콘으로 변경 (#15)

--- src/app/challenges/[id]/work/[workId]/_components/WorkDetailViewer.jsx
+++ src/app/challenges/[id]/work/[workId]/_components/WorkDetailViewer.jsx
@@ -2,9 +2,11 @@
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Image from '@tiptap/extension-image';
+import TextAlign from '@tiptap/extension-text-align';
+import { TextStyle } from '@tiptap/extension-text-style';
+import Color from '@tiptap/extension-color';
 import { useAuthStore } from '@/shared/store/useAuthStore';
 import * as styles from './WorkDetailViewer.css.js';
-import TextAlign from '@tiptap/extension-text-align';
 import '@/features/editor/TiptapEditor.css.js';
 
 function WorkContent({ content }) {
@@ -22,6 +24,8 @@ function WorkContent({ content }) {
       StarterKit,
       Image.configure({ inline: false, allowBase64: true }),
       TextAlign.configure({ types: ['heading', 'paragraph'] }),
+      TextStyle,
+      Color,
     ],
     content: parsedContent,
     editable: false,
---
+++ src/features/editor/store/useFormatStore.js
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+export const DEFAULT_FORMAT = {
+  bold: false,
+  italic: false,
+  underline: false,
+  textAlign: null,
+  bulletList: false,
+  orderedList: false,
+  color: '#000000',
+};
+
+export const useFormatStore = create((set) => ({
+  ...DEFAULT_FORMAT,
+  setFormat: (updates) => set((state) => ({ ...state, ...updates })),
+  resetFormat: () => set(DEFAULT_FORMAT),
+}));
